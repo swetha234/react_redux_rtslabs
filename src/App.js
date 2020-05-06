3822c7ac8edd40883c39e77a665297515ff8393a
swetha234 <hgutt001@odu.edu>
2020-05-06 19:27:12 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -4,95 +4,95 @@ import { setSearchValue, fetchData } from './actions/simpleAction';
 import './App.css';
 import { SearchResults } from './SearchResults';
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { searchvalue: ' ' };
-  }
+	constructor(props) {
+		super(props);
+		this.state = { searchvalue: ' ' };
+	}
 
-  onChangehandler = event => {
-    this.props.setSearchValue(event.target.value);
-  };
-  searchHackernews = event => {
-    const query = this.props.searchvalue;
-    this.props.fetchData(query);
-  };
+	onChangehandler = event => {
+		this.props.setSearchValue(event.target.value);
+	};
+	searchHackernews = event => {
+		const query = this.props.searchvalue;
+		this.props.fetchData(query);
+	};
 
-  render() {
-    return (
-      <div style={container}>
-        <div className='class1' style={headerStyle}>
-          <h1>HACKER NEWS</h1>
-          <h3>The most readable results on the web...!!!</h3>
-          <input
-            style={boxStyle}
-            type='text'
-            onChange={this.onChangehandler}
-            placeholder='Search stories....'
-          ></input>
-          <div>
-            <button style={buttonStyle} onClick={this.searchHackernews}>
-              Search{' '}
-            </button>{' '}
-          </div>
-        </div>
+	render() {
+		return (
+			<div style={container}>
+				<div className='class1' style={headerStyle}>
+					<h1>HACKER NEWS</h1>
+					<h3>The most readable results on the web...!!!</h3>
+					<input
+						style={boxStyle}
+						type='text'
+						onChange={this.onChangehandler}
+						placeholder='Search stories....'
+					></input>
+					<div>
+						<button style={buttonStyle} onClick={this.searchHackernews}>
+							Search{' '}
+						</button>{' '}
+					</div>
+				</div>
 
-        <div className='class2'>
-          <p
-            style={{
-              fontFamily: 'Helvetica',
-              fontSize: '20px',
-              marginLeft: '20px'
-            }}
-          >
-            Search Results...
-          </p>
+				<div className='class2'>
+					<p
+						style={{
+							fontFamily: 'Helvetica',
+							fontSize: '20px',
+							marginLeft: '20px'
+						}}
+					>
+						Search Results...
+					</p>
 
-          {this.props.results && <SearchResults data={this.props.results} />}
-        </div>
-      </div>
-    );
-  }
+					{this.props.results && <SearchResults data={this.props.results} />}
+				</div>
+			</div>
+		);
+	}
 }
 const mapStateToProps = state => ({
-  searchvalue: state.simpleReducer.searchvalue,
-  results: state.simpleReducer.results
+	searchvalue: state.simpleReducer.searchvalue,
+	results: state.simpleReducer.results
 });
 
 const mapDispatchToProps = dispatch => ({
-  setSearchValue: value => dispatch(setSearchValue(value)),
-  fetchData: value => dispatch(fetchData(value))
+	setSearchValue: value => dispatch(setSearchValue(value)),
+	fetchData: value => dispatch(fetchData(value))
 });
 
 //Styling
 
 const headerStyle = {
-  background: '#333',
-  color: '#fff',
-  textAlign: 'center',
-  padding: '70px',
-  fontFamily: 'Helvetica',
-  size: '35'
+	background: '#333',
+	color: '#fff',
+	textAlign: 'center',
+	padding: '70px',
+	fontFamily: 'Helvetica',
+	size: '35'
 };
 const boxStyle = {
-  width: '40%',
-  height: '30px',
-  borderRadius: '20px 20px',
-  fontFamily: 'Helvetica',
-  fontSize: '20px'
+	width: '40%',
+	height: '30px',
+	borderRadius: '20px 20px',
+	fontFamily: 'Helvetica',
+	fontSize: '20px'
 };
 
 const container = {
-  height: 'auto',
-  backgroundColor: '#f5eaea'
+	height: 'auto',
+	backgroundColor: '#f5eaea'
 };
 
 const buttonStyle = {
-  marginTop: '30px',
-  height: '30px',
-  width: '8%',
-  borderRadius: '20px 20px',
-  fontSize: '15px',
-  fontFamily: 'Helvetica'
+	marginTop: '30px',
+	height: '30px',
+	width: '8%',
+	borderRadius: '20px 20px',
+	fontSize: '15px',
+	fontFamily: 'Helvetica'
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/actions/simpleAction.js
+++ src/actions/simpleAction.js
@@ -1,19 +1,19 @@
 // import { render } from '@testing-library/react';
 
 export const setSearchValue = value => {
-  return {
-    type: 'SET_SEARCH_VALUE',
-    payload: value
-  };
+	return {
+		type: 'SET_SEARCH_VALUE',
+		payload: value
+	};
 };
 
 export const fetchData = value => dispatch => {
-  fetch('http://hn.algolia.com/api/v1/search?query=' + value)
-    .then(response => response.json())
-    .then(data => {
-      dispatch({
-        type: 'SET_SEARCH_RESULT',
-        payload: data
-      });
-    });
+	fetch('https://hn.algolia.com/api/v1/search?query=' + value)
+		.then(response => response.json())
+		.then(data => {
+			dispatch({
+				type: 'SET_SEARCH_RESULT',
+				payload: data
+			});
+		});
 };
